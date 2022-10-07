6f7878da8e272ca4fe9e31b4aa2672071d72fbb3
Hoddeas <fakehoddeas@gmail.com>
2022-10-07 13:28:13 -0600

    Update 1.0

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@
 let signInBtn = document.getElementById('sign-in-btn');
 let signUpBtn = document.getElementById('sign-up-btn');
 
+// Globlal Variable Users
+let users = loadUsers();
+
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener('click', signUpHandler);
 
@@ -16,4 +19,25 @@ signInBtn.addEventListener('click', signInHandler);
 
 function signInHandler() {
   console.log('Sign In Btn Clicked');
+}
+
+// HELPER FUNCTIONS
+
+// Create and return a new users array object
+function newUser(username, password) {
+  return {
+    username: username,
+    password: password
+  };
+}
+
+// Save users to localStorage
+function saveUsers() {
+  localStorage.setItem("users", JSON.stringify(users));
+}
+
+// Load users from localStorage
+function saveUsers() {
+  let usersStr = localStorage.getItem("users");
+  return JSON.parse(usersStr) ?? [];
 }
